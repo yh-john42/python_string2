1c6ffcbc10948a65687e7f60083ad109ff3baa51
mju-oss-class <mju-oss-class@users.noreply.github.com>
2018-10-21 23:57:53 +0900

    Create string.py

---
+++ string.py
@@ -0,0 +1,22 @@
+# python exercise #1 : data manipulation
+
+a = input()
+b = input()
+
+# a와 b의 길이를 출력
+
+
+# a와 b의 왼쪽, 오른쪽에 있는 빈칸 제거
+
+
+
+# a와 b를 대문자로 변경
+A = 
+B = 
+
+
+# A와 B를 결합하여 C를 만듬
+C = A + B
+
+
+# C를 출력
